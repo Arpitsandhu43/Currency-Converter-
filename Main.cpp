9d119ed5f8b82e2f7fdfd576b409a62245276582
Arpit Sandhu <165026782+Arpitsandhu43@users.noreply.github.com>
2024-03-26 19:30:09 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,9 +8,9 @@ void main()
        cout<<"Enter currency in American Dollars"<<endl;
        cin>>dollars;
        //Calculations
-       B=dollars*0.571505;
-       M=dollars*10.7956;
-       J=dollars*112.212;
+       B=dollars*0.79;
+       M=dollars*16.68;
+       J=dollars*151.42;
        Ind=dollars*83.31;
        cout<<dollars<<"American dollars are equal to "<<B<<" British pounds"<<endl;
        cout<<dollars<<"American dollars are equal to "<<M<<" Mexican pesos"<<endl;
